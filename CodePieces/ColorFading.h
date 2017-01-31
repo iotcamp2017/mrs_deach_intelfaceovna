f9fccb9032c0318e9aab3762761e84a7f73b1954
Andrey Ilichev <impulsgraw@ymail.com>
2017-01-31 11:31:36 +0300

    Color Fading Module

---
+++ CodePieces/ColorFading.h
@@ -0,0 +1,63 @@
+struct RGB
+{
+	unsigned char R;
+	unsigned char G;
+	unsigned char B;
+};
+
+struct HSL
+{
+	int H;
+	float S;
+	float L;
+};
+
+float HueToRGB(float v1, float v2, float vH)
+{
+	if (vH < 0)
+		vH += 1;
+
+	if (vH > 1)
+		vH -= 1;
+
+	if ((6 * vH) < 1)
+		return (v1 + (v2 - v1) * 6 * vH);
+
+	if ((2 * vH) < 1)
+		return v2;
+
+	if ((3 * vH) < 2)
+		return (v1 + (v2 - v1) * ((2.0f / 3) - vH) * 6);
+
+	return v1;
+}
+
+struct RGB HSLToRGB(struct HSL hsl) {
+	struct RGB rgb;
+
+	if (hsl.S == 0)
+	{
+		rgb.R = rgb.G = rgb.B = (unsigned char)(hsl.L * 255);
+	}
+	else
+	{
+		float v1, v2;
+		float hue = (float)hsl.H / 360;
+
+		v2 = (hsl.L < 0.5) ? (hsl.L * (1 + hsl.S)) : ((hsl.L + hsl.S) - (hsl.L * hsl.S));
+		v1 = 2 * hsl.L - v2;
+
+		rgb.R = (unsigned char)(255 * HueToRGB(v1, v2, hue + (1.0f / 3)));
+		rgb.G = (unsigned char)(255 * HueToRGB(v1, v2, hue));
+		rgb.B = (unsigned char)(255 * HueToRGB(v1, v2, hue - (1.0f / 3)));
+	}
+
+	return rgb;
+}
+
+
+struct RGB ConvertRangeToRGB(int num, int lightness){ 
+	struct HSL CLR = {num/100*360, 100, lightness/100*50};
+	
+	return HSLToRGB(CLR);
+}
